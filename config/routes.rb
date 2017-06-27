05ae35686d548a00ecf7486e70cd9cee83a7f824
Marcos <bocchio_marcos@hotmail.com>
2017-06-27 01:00:15 -0300

    practicado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'redes_sociales/facebook'
+
+  get 'redes_sociales/twitter'
+
+  get 'redes_sociales/google_mas'
+
+  resources :events
   get 'inicio/index'
-  root 'inicio#index'
+
+  root :to => 'inicio#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
